06542af9f4047dc05765e84d78ad7fdeaec2f7ca
mkpete <muturipter@gmail.com>
2024-04-27 01:30:03 +0300

    adding_icon changes

--- my_app/lib/main.dart
+++ my_app/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:my_app/screens/tasks_screen.dart'; // importing the task_screen.dart
+import 'package:flutter_svg/flutter_svg.dart'; // Import for SVG support
 
 void main() {
   runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -19,7 +20,14 @@ class MyApp extends StatelessWidget {
             .copyWith(primary: const Color(0xFF883007)), // Set primary color
         useMaterial3: true, // Enable Material 3 design elements
       ),
-      home: const TasksScreen(), // Set home screen to be  TasksScreen
+       // Set home screen to be  TasksScreen
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Todo List'),
+          leading: SvgPicture.asset('assets/app_icon.svg'), // Set the app icon here
+        ),
+        body: TasksScreen(),
+      ),
     );
   }
 }
